314699e4932f61b3ed97f81777940a31360616de
Tanish Baidya <tbaidya01@qub.ac.uk>
2024-11-12 00:24:56 +0000

    🚧 Add rough code for cv dashboard

---
+++ services/dashboardService.js
@@ -0,0 +1,98 @@
+const dayjs = require("dayjs");
+const fs = require("fs");
+
+class DashboardService {
+  constructor() {
+    this.datasource = "db/dashboard.json";
+    this.curriculumVitaeStoreFolder = "db/curriculum-vitae";
+  }
+
+  getCVUrl(fileName) {
+    return `/db/curriculum-vitae/${fileName}`;
+  }
+  readDashboard() {
+    try {
+      const data = fs.readFileSync(this.datasource, "utf8");
+      return JSON.parse(data);
+    } catch (err) {
+      console.error("Error reading dashboard:", err);
+      return [];
+    }
+  }
+
+  writeDashboard(dashboard) {
+    try {
+      fs.writeFileSync(this.datasource, JSON.stringify(dashboard, null, 2), "utf8");
+    } catch (err) {
+      console.error("Error writing dashboard:", err);
+    }
+  }
+
+  addCurriculumVita(cvFileBuffer, user) {
+    const dashboard = this.readDashboard();
+    const cvFileName = `${user.id}-${user.name.replace(/\s/g, "-")}.pdf`;
+    fs.writeFileSync(`${this.curriculumVitaeStoreFolder}/${cvFileName}`, cvFileBuffer);
+    const updatedDashboard = [
+      ...dashboard,
+      {
+        id: dashboard.length ? dashboard[dashboard.length - 1].id + 1 : 1,
+        userId: user.id,
+        cvFileName: cvFileName,
+        dateAdded: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        dateModified: null,
+      },
+    ];
+    this.writeDashboard(updatedDashboard);
+    return updatedDashboard[updatedDashboard.length - 1];
+  }
+
+  getCurriculumVitaeByUserId(userId) {
+    const dashboard = this.readDashboard();
+    if (!dashboard || dashboard.length === 0) {
+      return [];
+    }
+    return dashboard.find((cv) => cv.userId === userId);
+  }
+
+  getCurriculumVitaById(id) {
+    const dashboard = this.readDashboard();
+    return dashboard.find((cv) => cv.id === id);
+  }
+
+  updateCurriculumVita(id, cvFileBuffer) {
+    const dashboard = this.readDashboard();
+    const cvIndex = dashboard.findIndex((cv) => cv.id === id);
+    if (cvIndex === -1) {
+      console.error("Curriculum Vita not found");
+      return;
+    }
+
+    const cv = dashboard[cvIndex];
+    const cvFileName = cv.cvFileName;
+    fs.writeFileSync(`${this.curriculumVitaeStoreFolder}/${cvFileName}`, cvFileBuffer);
+    dashboard[cvIndex].dateModified = dayjs().format("YYYY-MM-DD HH:mm:ss");
+    this.writeDashboard(dashboard);
+    return dashboard[cvIndex];
+  }
+
+  deleteCurriculumVita(id) {
+    const dashboard = this.readDashboard();
+    if (!dashboard || dashboard.length === 0) {
+      console.error("Dashboard is empty");
+      return;
+    }
+    const cvIndex = dashboard.findIndex((cv) => cv.id === id);
+    if (cvIndex === -1) {
+      console.error("Curriculum Vita not found");
+      return;
+    }
+
+    const cv = dashboard[cvIndex];
+    fs.unlinkSync(`${this.curriculumVitaeStoreFolder}/${cv.cvFileName}`);
+    dashboard.splice(cvIndex, 1);
+    this.writeDashboard(dashboard);
+    return cv;
+  }
+}
+
+module.exports = DashboardService;
